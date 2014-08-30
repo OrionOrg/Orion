40d08e1249e4115e0993276f543b8e1192880769
Anthony <Anthony@Anthony-wis>
2014-08-30 19:37:55 +0800

    1

--- src/base-src/src/org/ratchetgx/orion/common/web/Log4jInit.java
+++ src/base-src/src/org/ratchetgx/orion/common/web/Log4jInit.java
@@ -13,17 +13,19 @@ public class Log4jInit extends HttpServlet {
     public void init() throws ServletException {
         
         String webAppRoot = getServletContext().getRealPath("/");
-        System.setProperty ("webAppRoot", webAppRoot);
         
-        String log4jConfigFilePath = webAppRoot + "/WEB-INF/classes/log4j.xml";
-        System.out.println("log4j配置文件路径为：" + log4jConfigFilePath);
-		File log4jFile = new File(log4jConfigFilePath);
-		if (log4jFile.exists()) {
-			System.out.println("日志配置文件存在!");
-			DOMConfigurator.configure(log4jConfigFilePath);
-		} else {
-			System.out.println("警告：日志配置文件不存在，请检查${WebAppRoot}/WEB-INF/classes/log4j.xml文件!");
-		}
+        System.setProperty ("webAppRoot", webAppRoot);
+        System.setProperty ("LOG_FILE_DIR", webAppRoot + File.separator + "logs" + File.separator);
+        
+//        String log4jConfigFilePath = webAppRoot + "/WEB-INF/classes/log4j.xml";
+//        System.out.println("log4j配置文件路径为：" + log4jConfigFilePath);
+//		File log4jFile = new File(log4jConfigFilePath);
+//		if (log4jFile.exists()) {
+//			System.out.println("日志配置文件存在!");
+//			DOMConfigurator.configure(log4jConfigFilePath);
+//		} else {
+//			System.out.println("警告：日志配置文件不存在，请检查${WebAppRoot}/WEB-INF/classes/log4j.xml文件!");
+//		}
 
     }
 
